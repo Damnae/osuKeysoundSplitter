74f5e005d3b59b11bd0ed8b199d3b6c4c415a8f6
Damnae <damnaee@gmail.com>
2014-09-07 17:16:34 +0200

    Fade the beginning and end of samples

--- src/com/damnae/osukeysoundsplitter/KeysoundWriter.java
+++ src/com/damnae/osukeysoundsplitter/KeysoundWriter.java
@@ -10,6 +10,9 @@ import com.damnae.osukeysoundsplitter.pathprovider.KeysoundPathProvider;
 import com.damnae.osukeysoundsplitter.strategy.KeysoundingStrategy;
 
 public class KeysoundWriter {
+	private static final int FADE_IN_DURATION = 2; // ms
+	private static final int FADE_OUT_DURATION = 2; // ms
+
 	private KeysoundingStrategy keysoundingStrategy;
 	private ExecutorService executorService;
 
@@ -26,6 +29,8 @@ public class KeysoundWriter {
 		final KeysoundPathProvider keysoundPathProvider = keysoundingStrategy
 				.getKeysoundPathProvider();
 		final AudioEncoder audioEncoder = keysoundingStrategy.getAudioEncoder();
+
+		fadeInOut(data, info);
 
 		String keysoundIdentifier = keysoundPathProvider.getIdentifier(data);
 		boolean isGenerated = keysoundPathProvider
@@ -57,5 +62,44 @@ public class KeysoundWriter {
 		}
 
 		return keysoundPath;
+	}
+
+	private void fadeInOut(byte[] data, AudioTrackInfo info) {
+		if (info.getBitsPerSample() != 16)
+			return;
+
+		int bytesPerSample = info.getBytesPerSample();
+		int sampleCount = data.length / bytesPerSample;
+
+		int fadeInSamples = Math.min(sampleCount / 2,
+				FADE_IN_DURATION * info.getSampleRate() / 1000);
+		int fadeOutSamples = Math.min(sampleCount / 2,
+				FADE_OUT_DURATION * info.getSampleRate() / 1000);
+
+		for (int offset = 0; offset < data.length; offset += bytesPerSample) {
+			int sample = offset / bytesPerSample;
+
+			double volume;
+			if (sample < fadeInSamples) {
+				volume = (double) sample / fadeInSamples;
+
+			} else if (sample > sampleCount - fadeOutSamples) {
+				volume = (double) (sampleCount - sample) / fadeOutSamples;
+
+			} else {
+				continue;
+			}
+
+			short left = (short) ((data[offset] & 0x00ff) | (data[offset + 1] << 8));
+			short right = (short) ((data[offset + 2] & 0x00ff) | (data[offset + 3] << 8));
+
+			left = (short) (left * volume);
+			right = (short) (right * volume);
+
+			data[offset] = (byte) (left & 0x00ff);
+			data[offset + 1] = (byte) ((left >> 8) & 0x00ff);
+			data[offset + 2] = (byte) (right & 0x00ff);
+			data[offset + 3] = (byte) ((right >> 8) & 0x00ff);
+		}
 	}
 }
